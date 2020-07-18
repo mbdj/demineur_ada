0a086af8dc1628bb67dda6dca07e134df5e317cc
mehdi <mbdj@free.fr>
2020-07-18 14:46:26 +0200

    modification de table2d en type tagged pour pouvoir bénéficier de la notation pointée des méthodes

--- src/main.adb
+++ src/main.adb
@@ -1,3 +1,4 @@
+with Ada.Text_IO; use Ada.Text_IO;
 with Table2d;
 
 with Type_case;  use Type_case;
@@ -43,4 +44,12 @@ begin
        Ligne   => 1,
        Colonne => 1,
        Item    => Type_case.VISIBLE);
+   grille_de_jeu.Set(Ligne => 1, Colonne => 1, Item    => Type_case.VISIBLE);
+   grille_de_jeu.Set(Ligne   => 2,
+                     Colonne => 3,
+                     Item    => Type_case.CACHE);
+
+   Put_Line (grille_de_jeu.Get(Ligne => 1, Colonne => 1)'Image);
+   Put_Line(grille_de_jeu.Get(Ligne => 2,Colonne => 3)'Image);
+
 end Main;
